91c28df54060332409cd05b4f21291078006dc2d
Program <Program@example.com>
2023-12-15 08:27:38 +0530

    first commit

---
+++ src/polymorphism/Main.java
@@ -0,0 +1,13 @@
+
+package polymorphism;
+
+public class Main {
+    public static void main(String [] args){
+        
+        Math m = new Math();
+        System.out.println(m.add(2,3 ));
+        System.out.println(m.add(2,3,5 ));
+        System.out.println(m.concat("poornima" ,"Katti"));
+             
+    }   
+}
---
+++ src/polymorphism/Math.java
@@ -0,0 +1,22 @@
+
+package polymorphism;
+
+public class Math {
+     public int add(int a, int b)
+     {
+         return a+b;
+    }
+     
+     public int add(int a, int b,int c)
+     {
+         return a+b+c;
+    }
+     
+public String concat (String a,String b)
+     {
+         return a+b;
+    }
+    
+}
+    
+
